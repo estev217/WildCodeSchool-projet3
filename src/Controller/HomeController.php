81fc9691d57b9141baaaf221b7254de9d1a8b818
Laureen Dumas <laureen262@hotmail.fr>
2019-12-03 17:40:30 +0100

    create homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,8 +3,18 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
-
+    /**
+     * @Route("/", name="index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('home.html.twig');
+    }
 }
