39899fb16e76b55eba40f24c774facb096a83102
Anne Quiedeville <anne.quiedeville@orange.fr>
2019-12-04 10:31:05 +0100

    ADD fixtures for roles and users

--- src/DataFixtures/RoleFixtures.php
+++ src/DataFixtures/RoleFixtures.php
@@ -3,8 +3,43 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Role;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
 
-class RoleFixtures
+class RoleFixtures extends Fixture
 {
 
+    const ROLES = [
+        'collab' => [
+            'name' => 'Collaborateur',
+            'identifier' => 'collab'
+        ],
+        'manager' => [
+            'name' => 'Manager',
+            'identifier' => 'manager'
+        ],
+        'admin' => [
+            'name' => 'Administrateur',
+            'identifier' => 'admin'
+        ],
+        ];
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $counter = 0;
+        foreach (self::ROLES as $data) {
+            $role = new Role();
+            $role->setName($data['name']);
+            $role->setIdentifier($data['identifier']);
+            $manager->persist($role);
+            $this->addReference('role_' . $counter, $role);
+            $counter++;
+        }
+        $manager->flush();
+    }
 }
