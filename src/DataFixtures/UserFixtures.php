37fe9888eeae1cd194d27efc35af2db3d645e3c2
Anne Quiedeville <anne.quiedeville@orange.fr>
2019-12-10 14:30:18 +0100

    update user fixtures to have collabs linked to managers

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -20,15 +20,40 @@ class UserFixtures extends Fixture
 
         $faker = Faker\Factory::create('fr_FR');
 
-        for ($i = 0; $i < 10; $i++) {
+        // Creates 1 admin
+        $user = new User();
+        $user->setFirstname($faker->firstName);
+        $user->setLastname($faker->lastName);
+        $user->setEmail($faker->email);
+        $user->setPassword($faker->password);
+        $user->setCreatedAt($faker->dateTime);
+        $user->setRole($this->getReference('role_2'));
+        $manager->persist($user);
+
+        // Creates 2 managers
+        for ($i = 0; $i < 2; $i++) {
             $user = new User();
             $user->setFirstname($faker->firstName);
             $user->setLastname($faker->lastName);
             $user->setEmail($faker->email);
             $user->setPassword($faker->password);
             $user->setCreatedAt($faker->dateTime);
-            $number = rand(0, 2);
-            $user->setRole($this->getReference('role_' . $number));
+            $user->setRole($this->getReference('role_1'));
+            $manager->persist($user);
+            $this->addReference('manager_' . $i, $user);
+        }
+
+        // Creates 7 collaborators associated to a manager
+        for ($i = 0; $i < 7; $i++) {
+            $user = new User();
+            $user->setFirstname($faker->firstName);
+            $user->setLastname($faker->lastName);
+            $user->setEmail($faker->email);
+            $user->setPassword($faker->password);
+            $user->setCreatedAt($faker->dateTime);
+            $user->setRole($this->getReference('role_0'));
+            $number = rand(0, 1);
+            $user->setManager($this->getReference('manager_' . $number));
             $manager->persist($user);
         }
         $manager->flush();
