8616dae013ea291f9d96b0c3490f5db28958ad98
Anne Quiedeville <anne.quiedeville@orange.fr>
2019-12-10 15:45:56 +0100

    refactor categories of checklist items to constants

--- src/DataFixtures/RoleFixtures.php
+++ src/DataFixtures/RoleFixtures.php
@@ -42,4 +42,55 @@ class RoleFixtures extends Fixture
         }
         $manager->flush();
     }
+
+    const STEPS = [
+        [
+            'name' => 'Intégration au siège',
+            'description' => '. Accueil par le responsable de pôle
+				                . Visite des locaux',
+            'duration' => 1,
+            ],
+        [
+            'name' => 'Intégration au siège',
+            'description' => '. Suivi du parcours de rencontre
+				                . Bilan des deux jours d\'intégration au siège avec le manager',
+            'duration' => 1,
+        ],
+        [
+            'name' => 'Intégration sur la résidence pilote - Mise en situation',
+            'description' => '. Mise en situation aux côtés du référent métier
+				                . Doit comprendre un week-end',
+            'duration' => 7,
+        ],
+        [
+            'name' => 'Intégration du lieu de travail',
+            'description' => '. Visite des locaux
+				                . Présentation de l\'équipe',
+            'duration' => 14,
+        ],
+        [
+            'name' => 'Suivi de l\'intégration',
+            'description' => '. Point avec les services supports
+				                . Suivi hebdomadaire',
+            'duration' => 14,
+        ],
+        [
+            'name' => 'Bilan de la période écoulée',
+            'description' => '. RDV bilan de la période écoulée',
+            'duration' => 1,
+        ],
+        [
+            'name' => 'Suivi mensuel',
+            'description' => '. Entretien de suivi avec le responsable de pôle
+				                . Si prolongation de la période d\'essai, 2nd entretien',
+            'duration' => 60,
+        ],
+        [
+            'name' => 'Bilan de la fin d\'intégration',
+            'description' => '. Entretien de suivi mensuel
+				                . Elaboration du plan de développement personnalisé',
+            'duration' => 180,
+        ],
+
+        ];
 }
