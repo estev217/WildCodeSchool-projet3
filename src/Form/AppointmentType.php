6eef48238dd7415bcdc1f638725c5a5cbbea0336
philippe <spartitus>
2020-01-08 15:29:13 +0100

    appointment crud

---
+++ src/Form/AppointmentType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Appointment;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AppointmentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'firstname',
+                ])
+            ->add('partner', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'firstname',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Appointment::class,
+        ]);
+    }
+}
