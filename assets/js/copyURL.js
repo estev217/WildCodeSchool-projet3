1b49031681efe459169153062682691d1d173d8a
Anne Quiedeville <anne.quiedeville@orange.fr>
2020-01-10 13:20:20 +0100

    add possibility to copy url of image

---
+++ assets/js/copyURL.js
@@ -0,0 +1,12 @@
+const copyButtons = document.getElementsByClassName('copy-button');
+const copyText = document.getElementsByClassName('copy-text');
+
+for (let i = 0; i < copyButtons.length; i++) {
+    copyButtons[i].addEventListener('click', function () {
+        copyText[i].select();
+        // const text = copyText[i].textContent;
+        document.execCommand('copy');
+        alert('Copied the text: ' + copyText[i].value);
+        }
+    );
+}
